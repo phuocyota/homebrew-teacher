7a94998d368ef1c97b455c9f04848c03f1f45a17
Phuoc Nguyen <nhphuoca7tqtpy@gmail.com>
2026-01-23 08:03:36 +0700

    Fix release asset URL

--- Casks/kido-teacher.rb
+++ Casks/kido-teacher.rb
@@ -2,7 +2,7 @@ cask "kido-teacher" do
   version "1.0.0"
   sha256 "54f2b63270adbae1f93a4a37fef6fb6419a1652f4f985a8b636ec1b69d21337b"
 
-  url "https://github.com/phuocyota/SimpleLoginApp/releases/download/v#{version}/Kido%20Teacher-macos.zip"
+  url "https://github.com/phuocyota/SimpleLoginApp/releases/download/v#{version}/Kido.Teacher-macos.zip"
   name "Kido Teacher"
   desc "Kido Teacher desktop app"
   homepage "https://github.com/phuocyota/SimpleLoginApp"
