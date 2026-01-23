720b253cb593d80f6a4c21242146781465c7e7a5
Phuoc Nguyen <nhphuoca7tqtpy@gmail.com>
2026-01-23 09:16:53 +0700

    Update kido-teacher to v1.0.1

--- Casks/kido-teacher.rb
+++ Casks/kido-teacher.rb
@@ -1,8 +1,8 @@
 cask "kido-teacher" do
-  version "1.0.0"
-  sha256 "54f2b63270adbae1f93a4a37fef6fb6419a1652f4f985a8b636ec1b69d21337b"
+  version "1.0.1"
+  sha256 "3ea982eba5977f8aad8e8ac13bd3d27fdcc9b1e3e4534a9386eb97ab65d69b60"
 
-  url "https://github.com/phuocyota/SimpleLoginApp/releases/download/v#{version}/Kido.Teacher-macos.zip"
+  url "https://github.com/phuocyota/SimpleLoginApp/releases/download/v#{version}/Kido-Teacher-macos.zip"
   name "Kido Teacher"
   desc "Kido Teacher desktop app"
   homepage "https://github.com/phuocyota/SimpleLoginApp"
