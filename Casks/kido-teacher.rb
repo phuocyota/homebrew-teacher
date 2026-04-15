6922a32c341d7eb4567439fa44ada3a7e9f6fd6b
phuocyota <nhphuoca7tqtpy@gmail.com>
2026-04-15 10:45:05 +0700

    Update kido-teacher to v2.0.1

--- Casks/kido-teacher.rb
+++ Casks/kido-teacher.rb
@@ -2,12 +2,12 @@ cask "kido-teacher" do
   version "2.0.1"
 
   on_arm do
-    sha256 "78186083fb84e5094f3cbb4ab99d265828f922589169022622bbe2c27e0c4ff0"
+    sha256 "52225e88c49e924a35a9072a7714dcda730dc51c842101ddb3917f88d6d76b34"
     url "https://github.com/phuocyota/SimpleLoginApp/releases/download/v#{version}/Kido-Teacher-macos-arm64.zip"
   end
 
   on_intel do
-    sha256 "dffeeecbdf8ea5ce83ca39ba9234cd9b255aa9f466e87223f74115a1841e00ca"
+    sha256 "36709c619e728c845eef800446dffaf7819a0095a6e0967a1992c9febb8307f3"
     url "https://github.com/phuocyota/SimpleLoginApp/releases/download/v#{version}/Kido-Teacher-macos-x64.zip"
   end
 
